5d8ef811e75ebabdb26934010d3200a1a452e5e4
najwanvan-netizen <najwanvan@gmail.com>
2026-09-22 11:19:31 +0700

    backend 05

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import { register, login } from '../controllers/authController.js';
 import { getTodos, createTodo } from '../controllers/todoController.js';
 import { validateRegister, validateLogin, validateTodo } from '../middlewares/validator.js';
-import { verifyToken } from '../middlewares/auth.Middleware.js';
+import { verifyToken } from '../middlewares/authMiddleware.js';
 
 const router = Router();
 
